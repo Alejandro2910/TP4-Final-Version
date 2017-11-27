490c33f42c7d294ff917c8706c13de132ba6d8b5
Alejandro2910 <32439030+Alejandro2910@users.noreply.github.com>
2017-11-27 07:43:45 -0600

    Add files via upload

--- include/ListaObjetos.h
+++ include/ListaObjetos.h
@@ -1,3 +1,12 @@
+/**
+    Tarea Programada 4
+    En esta tarea programada se manejarán archivos csv, se les generaran datos estadísticos
+    y literales dependiendo el tipo de dato.
+    Clase: Esta clase es la que contiene una lista con todos los objetos asociados al archivo
+    y además se encarga de obtener los datos literales y estadísticos de todos los objetos.
+    Autor: Alejandro Benavides-B61015
+    Version: v3
+**/
 #ifndef LISTAOBJETOS_H
 #define LISTAOBJETOS_H
 #include <list>
