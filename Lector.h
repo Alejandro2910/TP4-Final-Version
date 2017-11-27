07517d4ae2a08039b72b3edef5813e33d35e29f9
Alejandro2910 <32439030+Alejandro2910@users.noreply.github.com>
2017-11-27 14:54:22 -0600

    Add files via upload

---
+++ Lector.h
@@ -0,0 +1,51 @@
+/**
+    Tarea Programada 4
+    En esta tarea programada se manejarán archivos csv, se les generaran datos estadísticos y literales dependiendo el tipo de dato.
+    Clase: La clase lector es la clase encargada de la lectura, escritura y generacion de datos probabilísticos y literales del archivo leido.
+    Autor: Alejandro Benavides-B61015
+    Version: v3
+    @param ifstream lect es el inpute file stream por el cual ingresan los datos.
+    @param ofstream escr es el output file stream por el cual salen los datos.
+    @param vector<string> nombresObj guarda los nombres de los parametros leídos del archivo.
+    @param vector<string> tipoDatos guarda los tipos de dato de los parametros leídos del archivo.
+    @param stringstream datosGenerados guarda los datos generados para luego ser escritos en el archivo de escritura.
+**/
+#ifndef LECTOR_H
+#define LECTOR_Hd
+#include <istream>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include "Objeto.h"
+#include "ListaObjetos.h"
+#include <vector>
+#include <list>
+
+using namespace std;
+
+class Lector
+{
+    public:
+        ifstream lect;
+        ofstream escr;
+        vector<string> nombresObj;
+        vector<string> tipoDatos;
+        stringstream datosGenerados;
+
+        void leaObjetos(list<Objeto>&);
+        void leaNombres();
+        void leaTipo();
+        void abraParaLeer(string);
+        void cierreLeido();
+        void escriba();
+        void abraParaEscribir(string);
+        void cierreEscrito();
+        void genereDatos(ListaObjetos&);
+
+
+    protected:
+
+    private:
+};
+
+#endif // LECTOR_H
---
+++ ListaObjetos.h
@@ -0,0 +1,37 @@
+/**
+    Tarea Programada 4
+    En esta tarea programada se manejarán archivos csv, se les generaran datos estadísticos
+    y literales dependiendo el tipo de dato.
+    Clase: Esta clase es la que contiene una lista con todos los objetos asociados al archivo
+    y además se encarga de obtener los datos literales y estadísticos de todos los objetos.
+    Autor: Alejandro Benavides-B61015
+    Version: v3
+    @param list<Objeto> listaDeObjetos es una lista que guarda todos los objetos leídos del archivo.
+
+**/
+#ifndef LISTAOBJETOS_H
+#define LISTAOBJETOS_H
+#include <list>
+#include "Objeto.h"
+#include <sstream>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+class ListaObjetos
+{
+    public:
+        list<Objeto> listaDeObjetos;
+        ListaObjetos();
+        virtual ~ListaObjetos();
+        string muestreObjetos();
+        string getDatosNumericos(int);
+        string getDatosLiterales(int);
+
+    protected:
+
+    private:
+};
+
+#endif // LISTAOBJETOS_H
